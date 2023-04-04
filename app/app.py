fde1aa54a13974f4b8000e64e81489f2b1318a4f
fernetsystem <fernetsystem@gmail.com>
2023-04-04 01:01:10 -0600

    ls

--- app/app.py
+++ app/app.py
@@ -5,6 +5,16 @@ app=Flask(__name__)
 @app.route('/')
 def index():
 	#return '<h2>Wecolme</h2>'
-	return render_template('index.html')
+	products = ['Applications WEB', 'Applications Mobile', 'Cloud Computing', 'Migrations (on premise - cloud)', 'Blockchain']
+
+	data = {
+		'tittle': 'VECSA',
+		'introduction': 'Technology on demand',
+		'products': products,
+		'total_products': len(products)
+	}
+
+
+	return render_template('index.html', data=data)
 if __name__=='__main__':
 	app.run(debug=True,host='0.0.0.0', port=80)
