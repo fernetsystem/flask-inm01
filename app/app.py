fb6f59aa177205f1c36d64cfc0ff23fc904a3fec
fernetsystem <fernetsystem@gmail.com>
2023-04-04 18:41:56 -0600

    consume api test

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_foor, flash, session
+import json
 app=Flask(__name__)
 
 @app.route('/')
@@ -14,7 +14,22 @@ def index():
 		'total_products': len(products)
 	}
 
-
+	url = "was"
+	headers = {'Authorization': ''}
+	response = request.request('POST', url, header=headers)
+	data = json.loads(response.read())
+	print(data)
+	
 	return render_template('index.html', data=data)
+@app.route('/getData')
+def getData():	
+	url = "was"
+	headers = {'Authorization': ''}
+	response = request.request('POST', url, header=headers)
+	data = json.loads(response.read())
+	print(data)
+	return render_template('index.html', data=data)
+
+	
 if __name__=='__main__':
 	app.run(debug=True,host='0.0.0.0', port=80)
