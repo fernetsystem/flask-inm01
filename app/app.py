92a0d734d928b471600a56dad89f896208172d9d
fernetsystem <fernetsystem@gmail.com>
2023-04-04 19:49:55 -0600

    add datajson in template

--- app/app.py
+++ app/app.py
@@ -8,20 +8,24 @@ app=Flask(__name__)
 @app.route('/')
 def index():
 	#return '<h2>Wecolme</h2>'
+
+	url = "http://"
+	headers = {'Authorization': ''}
+	response = requests.request('POST', url, headers=headers)
+	
+	print(response.json())
+
 	products = ['Applications WEB', 'Applications Mobile', 'Cloud Computing', 'Migrations (on premise - cloud)', 'Blockchain']
 
 	data = {
 		'tittle': 'VECSA',
 		'introduction': 'Technology on demand',
 		'products': products,
-		'total_products': len(products)
+		'total_products': len(products),
+		'jsonData': response.json()
 	}
 
-	url = "http://"
-	headers = {'Authorization': ''}
-	response = requests.request('POST', url, headers=headers)
 	
-	print(response.json)
 	
 	return render_template('index.html', data=data)
 @app.route('/getData')
